b9c1c9172ea6f6a41109b48d00023177a3ae6e21
kostitsyn <kostitsin.a@mail.ru>
2021-09-02 15:03:00 +0400

    Урок 1

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#Django_rest_framework
-=======
 # Django_rest_framework
->>>>>>> 2a44056eafe5c7c8a62622d660d4ec9adbf3b9b2
--- authors/models.py
+++ authors/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from uuid import uuid4
+import markdown
+import django_filters
+
 
 class Author(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
