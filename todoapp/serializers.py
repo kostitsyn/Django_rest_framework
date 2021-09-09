c007e5e402e95b984d508b7dd58248e573569d4e
kostitsyn <kostitsin.a@mail.ru>
2021-09-09 10:29:14 +0400

    Третье домашнее задание

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -1,10 +1,12 @@
-from rest_framework.serializers import ModelSerializer, StringRelatedField
+from rest_framework.serializers import ModelSerializer, StringRelatedField, HyperlinkedRelatedField
 from .models import Project, ToDo
 from usersapp.serializers import UserModelSerializer
 
 
 class ProjectSerializer(ModelSerializer):
-    users = UserModelSerializer(many=True)
+    # users = UserModelSerializer(many=True)
+    # users = HyperlinkedRelatedField(many=True, view_name='user-detail', read_only=True)
+    users = StringRelatedField(many=True)
 
     class Meta:
         model = Project
@@ -12,7 +14,9 @@ class ProjectSerializer(ModelSerializer):
 
 
 class ToDoSerializer(ModelSerializer):
-    project = StringRelatedField()
+    # project = StringRelatedField()
+    # project = ProjectSerializer()
+    project = HyperlinkedRelatedField(view_name='project-detail', read_only=True)
     user = StringRelatedField()
 
     class Meta:
