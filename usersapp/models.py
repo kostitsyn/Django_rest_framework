899a97536ae7cc7de453ca415f6e5d4c855c79f6
kostitsyn <kostitsin.a@mail.ru>
2021-09-22 19:58:14 +0400

    Восьмое домашнее задание

--- usersapp/models.py
+++ usersapp/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ['username']
 
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
--- usersapp/tests.py
+++ usersapp/tests.py
@@ -1,3 +1,45 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
+from mixer.backend.django import mixer
+from django.contrib.auth import models
+from .views import UserModelViewSet
+from .models import User
 
-# Create your tests here.
+
+class TestUserViewSet(TestCase):
+
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        self.admin = models.User.objects.create_superuser(username='django', password='21778821')
+
+    def test_get_user_list(self):
+        request = self.factory.get('/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_user_un(self):
+        user = User.objects.create(username='JD', firstname='John', lastname='Doe', email='doe@john.ru')
+        request = self.factory.put(f'/api/users/{user.uuid}/', {'firstname': 'Igor',
+                                                                    'lastname': 'Borisovich'})
+        view = UserModelViewSet.as_view({'put': 'update'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # def test_update_user(self):
+    #     user = User.objects.create(username='JD', firstname='John', lastname='Doe', email='doe@john.ru')
+    #     request = self.factory.put(f'/api/users/{user.uuid}/', {'firstname': 'Igor',
+    #                                                                 'lastname': 'Borisovich'})
+    #     force_authenticate(request, self.admin)
+    #     view = UserModelViewSet.as_view({'put': 'update'})
+    #     response = view(request)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_current_user(self):
+        user = User.objects.create(username='JD', firstname='John', lastname='Doe', email='doe@john.ru')
+        request = self.factory.get(f'/api/users/{user.uuid}/')
+        view = UserModelViewSet.as_view({'get': 'retrieve'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
