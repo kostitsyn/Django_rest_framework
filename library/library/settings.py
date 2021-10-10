dd9fc52347c863b345c0047c86bc300ac0a3ea57
kostitsyn <kostitsin.a@mail.ru>
2021-10-10 19:28:06 +0400

    Урок 12

--- library/library/settings.py
+++ library/library/settings.py
@@ -96,9 +96,17 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library',
+        'USER': 'aleksandr',
+        'PASSWORD': '21778821',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
