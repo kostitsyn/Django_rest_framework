535ffde3c4f72736e190f3d295a188fcfb927129
kostitsyn <kostitsin.a@mail.ru>
2021-10-16 19:00:21 +0400

    To deploy

--- todoservice/todoservice/settings.py
+++ todoservice/todoservice/settings.py
@@ -30,9 +30,9 @@ with open('secret.json') as f:
         SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
